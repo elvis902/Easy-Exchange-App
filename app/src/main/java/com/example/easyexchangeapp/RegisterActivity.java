df45cbb9fb9ca9d4718161b2f9abe1c0b0138925
Mriganka Bharali <mrigankabharali30@gmail.com>
2020-11-05 00:55:00 +0530

    Colors added (Register)

--- app/src/main/java/com/example/easyexchangeapp/RegisterActivity.java
+++ app/src/main/java/com/example/easyexchangeapp/RegisterActivity.java
@@ -10,38 +10,11 @@ import android.widget.TextView;
 
 public class RegisterActivity extends AppCompatActivity {
 
-    Button mRegisterBtn;
-    TextView mRegisterTextView;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-
-
-        mRegisterBtn = findViewById(R.id.register_btn);
-        mRegisterTextView = findViewById(R.id.have_account);
-
-
-
-
-
-        mRegisterBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(RegisterActivity.this,MainActivity.class));
-            }
-        });
-
-
-        mRegisterTextView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(RegisterActivity.this,LoginActivity.class));
-            }
-        });
-
 
 
     }
