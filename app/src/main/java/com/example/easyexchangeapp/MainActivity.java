a88a1e2c7fa01d5d675ee8d57d23626ff3629ad4
Mriganka Bharali <mrigankabharali30@gmail.com>
2020-11-22 13:45:37 +0530

    Authentication Added

--- app/src/main/java/com/example/easyexchangeapp/MainActivity.java
+++ app/src/main/java/com/example/easyexchangeapp/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.easyexchangeapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button logOut;
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+        logOut = findViewById(R.id.logOut);
+        logOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAuth.signOut();
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                finish();
+            }
+        });
     }
 }
