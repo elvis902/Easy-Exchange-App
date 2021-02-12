41512399f35e1ada20fc84456bb3a40d0b9e5bb5
sk25469 <sk25469@gmail.com>
2021-02-12 13:11:17 +0530

    implemented home items view in recycle view, added drawables

--- app/src/main/java/com/example/easyexchangeapp/MainActivity.java
+++ app/src/main/java/com/example/easyexchangeapp/MainActivity.java
@@ -1,14 +1,37 @@
 package com.example.easyexchangeapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.easyexchangeapp.myAdapter.ItemAdapter;
+
+public class MainActivity extends AppCompatActivity {
+    RecyclerView recyclerView;
+
+    String price[] = {"569", "Rs. 4588", "Rs. 8970", "Rs. 1999", "169", "123", "458", "897", "794", "169"},
+            description[] = {"Water Resistance Travel Bag", "Asus Zenfone Max", "Comfy Double Bed", "Oneplus Bullet Wireless", "mno", "abc", "def", "ghi", "jkl", "mno"},
+            address[] = {"PG Hostel, NITS", "Hostel 2, NITS", "Dean Office, NITS", "Hostel 7, NITS", "uegieoihg", "qwe", "rtyy", "uiop", "gaiwhgafdf", "uegieoihg"};
+    int images[] = {R.drawable.bag,R.drawable.phone,R.drawable.bed,
+            R.drawable.hp,R.drawable.pen,R.drawable.pen,
+            R.drawable.bag,R.drawable.bag,R.drawable.bag,
+            R.drawable.pen};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //price = getResources().getStringArray(R.array.price);
+        //description = getResources().getStringArray(R.array.description);
+        //address = getResources().getStringArray(R.array.address);
+
+        ItemAdapter itemAdapter = new ItemAdapter(this, price, description, address, images);
+
+        recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setAdapter(itemAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
     }
 }
---
+++ app/src/main/java/com/example/easyexchangeapp/myAdapter/ItemAdapter.java
@@ -0,0 +1,62 @@
+package com.example.easyexchangeapp.myAdapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.easyexchangeapp.R;
+
+public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.MyViewHolder> {
+
+    String data1[], data2[], data3[];
+    int images[];
+    Context context;
+    public ItemAdapter(Context ct, String price[], String description[], String address[], int img[]){
+        context = ct;
+        data1 = price;
+        data2 = description;
+        data3 = address;
+        images = img;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view =  inflater.inflate(R.layout.home_items_view, parent, false);
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        holder.itemPrice.setText(data1[position]);
+        holder.itemDescription.setText(data2[position]);
+        holder.itemAddress.setText(data3[position]);
+        holder.itemImage.setImageResource(images[position]);
+    }
+
+    @Override
+    public int getItemCount() {
+        return data1.length;
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder{
+
+        TextView itemPrice, itemDescription, itemAddress;
+        ImageView itemImage;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            itemPrice = itemView.findViewById(R.id.itemPrice);
+            itemDescription = itemView.findViewById(R.id.itemDescription);
+            itemAddress = itemView.findViewById(R.id.itemAddress);
+            itemImage = itemView.findViewById(R.id.itemImage);
+        }
+    }
+}
