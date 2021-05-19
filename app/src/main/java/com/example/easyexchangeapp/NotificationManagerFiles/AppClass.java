17023160838354d55bf01685deac125213ea939b
Snehashish2403 <snehashishkarmakar2403@gmail.com>
2021-05-19 20:57:21 +0530

    Debug

---
+++ app/src/main/java/com/example/easyexchangeapp/NotificationManagerFiles/AppClass.java
@@ -0,0 +1,30 @@
+package com.example.easyexchangeapp.NotificationManagerFiles;
+
+import android.app.Application;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
+
+public class AppClass extends Application {
+    public static final String Channel_ID="notificationServiceChannel";
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        createNotificationChannel();
+    }
+
+    private void createNotificationChannel(){
+        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.O){
+            NotificationChannel serviceChannel=new NotificationChannel(
+                    Channel_ID,
+                    "Easy Exchange App",
+                    NotificationManager.IMPORTANCE_DEFAULT
+            );
+            NotificationManager manager=getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(serviceChannel);
+        }
+    }
+}
