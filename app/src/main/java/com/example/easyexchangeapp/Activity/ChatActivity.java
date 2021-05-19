5bfa67e7243f702dd086fd84c8185e47143abfea
Snehashish2403 <snehashishkarmakar2403@gmail.com>
2021-05-19 21:08:37 +0530

    Resolved some issues

--- app/src/main/java/com/example/easyexchangeapp/Activity/ChatActivity.java
+++ app/src/main/java/com/example/easyexchangeapp/Activity/ChatActivity.java
@@ -1,6 +1,7 @@
 package com.example.easyexchangeapp.Activity;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -21,9 +22,10 @@ import com.example.easyexchangeapp.Adapters.ChatAdapter;
 import com.example.easyexchangeapp.Adapters.ItemAdapter;
 import com.example.easyexchangeapp.Constants.Constants;
 import com.example.easyexchangeapp.Models.ChatModel;
-import com.example.easyexchangeapp.Models.RegisterUser;
+import com.example.easyexchangeapp.NotificationManagerFiles.NotificationServiceClass;
 import com.example.easyexchangeapp.R;
 import com.example.easyexchangeapp.SharedPrefManager.SharedPrefManager;
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -41,8 +43,6 @@ public class ChatActivity extends AppCompatActivity {
     private EditText messageBody;
     private ImageButton sendButton;
     private ChatAdapter chatAdapter;
-    private Class<? extends DatabaseReference> receiver;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -52,14 +52,12 @@ public class ChatActivity extends AppCompatActivity {
         messageBody=findViewById(R.id.chatMessageText);
         sendButton=findViewById(R.id.chatSendButton);
 
-        SharedPrefManager manager=new SharedPrefManager(getApplicationContext());
 
+
+        SharedPrefManager manager=new SharedPrefManager(getApplicationContext());
 
         Intent intent=getIntent();
         clientName=intent.getStringExtra("client");
-
-        databaseReference = FirebaseDatabase.getInstance().getReference().child("Users").child(clientName);
-        
         userName=manager.getValue(Constants.USER_NAME);
         chatRoomId=intent.getStringExtra("chat-room");
         userId=manager.getValue(Constants.USER_ID);
@@ -86,14 +84,20 @@ public class ChatActivity extends AppCompatActivity {
             }
         });
 
+
+
         databaseReference= FirebaseDatabase.getInstance().getReference().child("Chat-Rooms").child(chatRoomId);
+        databaseReference.child("search").setValue(chatRoomId);
         databaseReference.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 List<ChatModel> chatList=new ArrayList<>();
                 for(DataSnapshot temp: snapshot.getChildren()){
-                    chatList.add(temp.getValue(ChatModel.class));
-                    System.out.println("message: "+temp.getValue(ChatModel.class).getMessage());
+                    if(temp.getKey().equals("search")||temp.getKey().equals("sender")){
+                        continue;
+                    }else{
+                        chatList.add(temp.getValue(ChatModel.class));
+                    }
                 }
                 chatAdapter.setChatModelList(chatList);
                 chatAdapter.notifyDataSetChanged();
@@ -110,14 +114,21 @@ public class ChatActivity extends AppCompatActivity {
             public void onClick(View v) {
                 String message=messageBody.getText().toString();
                 if(!message.equals("")){
+                    databaseReference.child("sender").setValue(userId);
                     databaseReference.push().setValue(new ChatModel(userName,message,userId));
                     messageBody.setText("");
                 }
             }
         });
-        
+
     }
 
-
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Intent notificationServiceIntent=new Intent(this, NotificationServiceClass.class);
+        notificationServiceIntent.putExtra("temp",chatRoomId);
+        startService(notificationServiceIntent);
+    }
 
 }
